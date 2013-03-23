2b6cd7bedaec53414dd7a149e51c95b041c3e345
Matt Schofield <matt.schofield.102@gmail.com>
2013-03-23 17:52:01 +0000

    FUBAR

--- public/javascripts/mapper.js
+++ public/javascripts/mapper.js
@@ -1,5 +1,43 @@
-function draw(pos) {
+
+function drawRoute(map, arr) {
+  var origin = destination = null;
+  var waypoints = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (i==0) { origin = arr[i].location; }
+    else if (i==arr.length-1) { destination = arr[i].location; }
+    else {
+      waypoints.push(arr[i]);
+    }
+  };
+
+  var request = {
+    origin: origin,
+    destination: destination,
+    waypoints: waypoints,
+    travelMode: google.maps.DirectionsTravelMode.DRIVING
+  };
   
+  console.log("ORIGIN: "+origin);
+  console.log("DESTINATION: "+destination);
+
+  var rendererOptions = { map: map, suppressMarkers: false };
+  directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+  directionsService = new google.maps.DirectionsService();
+  directionsService.route(request, function(response, status) {
+    // var duration = response.routes[0].legs[0].duration.text;
+    console.log(status);
+    console.log(response);
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+      console.log("Route should be plotted");
+    } else {
+      console.log('failed to get directions');
+    }
+  })
+  waypoints = [];
+}
+
+function draw() {
   //Load Google Map
   var latlng = new google.maps.LatLng(53.477333 , -2.247412);
   var myOptions = {
@@ -8,7 +46,7 @@ function draw(pos) {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-  
+
   $.get("http://localhost:3000/buses")
   .done(function(data) {
     data.forEach(function(el,i,arr) {
@@ -28,6 +66,40 @@ function draw(pos) {
       })
 
     })
+  });
+
+  $.get("http://localhost:3000/stops/1")
+  .done(function(data) {
+    var origin = null,
+        destination = null,
+        waypoints = [],
+        allpoints = [];
+
+    data.forEach(function(el,i,arr) {
+      allpoints.push({
+        location: new google.maps.LatLng(el.Latitude, el.Longitude)
+      });
+    })
+
+    var lastDestination = null;
+    var tmp = []
+    while (allpoints.length!=0) {
+      while (tmp.length != 10 && allpoints.length != 0) {
+        if (tmp.length == 9) {
+          tmp.push(allpoints[0]);
+        } else {
+          tmp.push(allpoints.shift());
+        }
+      }
+      console.log("TMP: "+tmp.length);
+      console.log("AP: "+allpoints.length);
+
+      drawRoute(map, tmp);
+      // while (temp.length) { tmp.shift(); }
+      tmp = [];
+    }
+    // allpoints = [];
+
   });
 }
 
