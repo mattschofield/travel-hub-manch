b79a78e138e8a418607eaffd2677e7603cf84c39
Karl <karlbarker1@gmail.com>
2013-03-23 12:50:17 +0000

    Added all stops route

--- routes/stops.js
+++ routes/stops.js
@@ -1,40 +1,49 @@
 
 /*
- * GET buses listing.
+ * GET stops listing.
  */
 var request = require('request');
 var config = require('../config/config');
 var async = require('async');
-var buses = [];
+var stops = [];
 
 exports.list = function(req, res){
-  
-  
   async.parallel([
     function(callback) {
-      getbus(1, function(err) {
+      getStops(1, function(err) {
         if (!err) { callback(null, 1); }
       })
     },
     function(callback) {
-      getbus(2, function(err) {
+      getStops(2, function(err) {
         if (!err) { callback(null, 2); }
       })
     },
     function(callback) {
-      getbus(3, function(err) {
+      getStops(3, function(err) {
         if (!err) { callback(null, 3); }
       })
     }
   ], function(err, results){
     res.type('application/json');
-    res.json(buses);
+    res.json(stops);
   });
 };
 
-function getbus(id, callback){
+function buildStopTasks(){
+  var tasks = [];
+  for (var i=1; i<4; i++){
+    tasks.push(function(callback) {
+      getStops(i, function(err) {
+        if (!err) { callback(null, i); }
+      })
+    })
+  }
+}
+
+function getStops(routeId, callback){
   var options = {
-    url: config.apiAddr + '/api/routes/MET'+id+'/buses',
+    url: config.apiAddr + '/api/routes/'+routeId+'/stops',
     headers: {
       DevKey:config.devKey,
       AppKey:config.appKey
@@ -43,7 +52,7 @@ function getbus(id, callback){
   request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       JSON.parse(body).forEach(function(el, i, arr){
-        buses.push(el);      
+        stops.push(el);      
       });
       callback(null);
     }
