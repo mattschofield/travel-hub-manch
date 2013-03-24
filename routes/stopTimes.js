80a116886cb871c1c3b4878b3dd886792a05c482
Karl <karlbarker1@gmail.com>
2013-03-24 14:03:30 +0000

    times

--- routes/stopTimes.js
+++ routes/stopTimes.js
@@ -18,61 +18,24 @@ exports.listByStop = function(req, res){
 
 function getTimes(stopId, times, callback){
   console.log("Getting times for s_id : "+stopId);
-  // url = "http://gtfsapi.herokuapp.com/stops?stop_id="+stopId+"&include_stop_times=true"
-  // request(url, function (error, response, body) {
-  //   if (!error && response.statusCode == 200) {
-  //     var parsedTimes = JSON.parse(body).stop_times
-  //     var now = moment().year(2000).dayOfYear(1);
+  url = "http://gtfsapi.herokuapp.com/stops?stop_id="+stopId+"&include_stop_times=true&from=now&stop_time_limit=5"
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var parsedTimes = JSON.parse(body).stops[0].stop_times
+      var now = moment().year(2000).dayOfYear(1);
 
-  //     for (var i = 0; i < parsedTimes.length; i++) {
-  //       var checkTime = moment(parsedTimes[i].departure_time).year(2000).dayOfYear(1);
-  //       if (checkTime.isBefore(now)) {
-
-  //       } else {
-  //         allDataFromAPI = parsedTimes.splice(i, 5);
-  //         console.log(allDataFromAPI);
-  //         allDataFromAPI.forEach(function(el, i, arr){
-  //           time = {
-  //             routeName: el.trip.route.route_short_name,
-  //             depTime: moment(el.departure_time).format("HH:mm"),
-  //             expectedIn: moment(el.departure_time).from(now)
-  //           }
-  //           times.push(time);
-  //         })
-  //         break;
-  //       }
-  //     };
-  //     console.log(times);
-  //     callback(null, times);
-  //   }
-  // });
-  tmpTimes = [
-    {
-      routeName: "MET1"
-      ,depTime: "11:45"
-      ,expectedIn: "1 min"
-    },
-    {
-      routeName:"MET1"
-      ,depTime: "11:48"
-      ,expectedIn: "4 mins"
-    },
-    {
-      routeName: "MET1"
-      ,depTime: "12:00"
-      ,expectedIn: "16 mins"
-    },
-    {
-      routeName: "MET1"
-      ,depTime: "12:02"
-      ,expectedIn: "18 mins"
-    },
-    {
-      routeName: "MET1"
-      ,depTime: "12:15"
-      ,expectedIn: "30 mins"
+      for (var i = 0; i < parsedTimes.length; i++) {
+        var checkTime = moment(parsedTimes[i].departure_time, "HH:mm:ss").year(2000).dayOfYear(1);
+        time = {
+          routeName: parsedTimes[i].trip.route.route_short_name.trim(),
+          depTime: checkTime.format("HH:mm"),
+          expectedIn: checkTime.from(now)
+        }
+        times.push(time);
+      };
+      console.log(times);
+      callback(null, times);
     }
-  ];
-  callback(null, tmpTimes);
+  });
   
 };
